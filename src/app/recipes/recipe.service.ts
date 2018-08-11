7e1fd822e8affa3c4abb1076d5d31ec081a90f17
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-08-10 22:41:41 -0700

    Added more ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,16 +10,44 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
   private recipes: Recipe[] = [
-    new Recipe('In-N-Out',
-      'At In-N-Out Burger, quality is everything.',
-      'https://cdn.vox-cdn.com/thumbor/6QUP72lS3lPRk7ztA5vPtrqwvq8=/0x0:4519x2758/1820x1213/filters:focal(1899x1018:2621x1740):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60033615/20832096314_7323ec81cc_o.0.jpg',
+    new Recipe('Budae Jjigae',
+      'Korean Army Stew, a popular Korean hot pot dish',
+      'http://iamafoodblog.com/wp-content/uploads/2017/09/korean-army-stew-8734.jpg',
       [
         new Ingredient('Meat', 2),
         new Ingredient('French Fries', 35 )
       ]),
-    new Recipe('Roscoe\'s Chicken and Waffles',
-      'Fried chicken and waffles are our specialty!',
-      'https://lasentinel.net/wp-content/uploads/sites/5/2018/01/LOC-roscoes.jpg',
+    new Recipe('Middle Eastern Shakshuka',
+      'Shakshuka is top of the list of my favorite egg dishes.',
+      'https://www.themediterraneandish.com/wp-content/uploads/2017/01/Shakshuka-Recipe-The-Mediterranean-Dish-100.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Waffles', 1 )
+      ]),
+    new Recipe('Skillet Bacon Cheeseburger Dough Ball Dip',
+      'Bacon Cheeseburger dough ball dip is a perfect dish for game days!',
+      'https://cookingtv-channel.com/wp-content/uploads/37947843_1976187535753766_3372326085080383488_n.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Waffles', 1 )
+      ]),
+    new Recipe('5-Ingredient Sheet Pan Salmon',
+      'Perfect dinner for busy weeknights',
+      'https://www.platingsandpairings.com/wp-content/uploads/2017/06/5-ingredient-sheet-pan-salmon-2.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Waffles', 1 )
+      ]),
+    new Recipe('Garlic Butter Shrimp',
+      'Best if served over pasta or with a lightly toasted baguette',
+      'http://images.huffingtonpost.com/2014-10-19-GarlicButterShrimp.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Waffles', 1 )
+      ]),
+    new Recipe('Broiled Asian-Style Flat Iron Steak',
+      'Best if served with buttered rice and steamed vegetables.',
+      'http://images.huffingtonpost.com/2014-10-19-AsianStyleFlatIronSteak.jpg',
       [
         new Ingredient('Chicken', 2),
         new Ingredient('Waffles', 1 )
